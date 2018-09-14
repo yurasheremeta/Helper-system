9a2972708f8acb9eb038d756f4c095a747796fc5
yurasheremeta <sheremeta.yura@gmail.com>
2018-09-15 01:25:14 +0300

    added JWTTokenFilterConfigurer

--- src/main/java/ua/logos/configJWT/JWTTokenFilter.java
+++ src/main/java/ua/logos/configJWT/JWTTokenFilter.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
 public class JWTTokenFilter extends GenericFilterBean{
 	@Autowired
 	private JWTTokenProvider jwtTokenProvider;
